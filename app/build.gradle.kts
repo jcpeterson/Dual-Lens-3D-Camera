ee82dd74267450cf9d819d50c03a64cb103aed0e
Joshua Peterson <8072691+jcpeterson@users.noreply.github.com>
2026-01-20 10:11:29 -0500

    mark 0.42 formally

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 4
-        versionName = "0.41"
+        versionName = "0.42"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
