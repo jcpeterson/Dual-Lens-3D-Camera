90ad2b99812ab1bf46d0cc33411193e93e086dfb
Joshua Peterson <8072691+jcpeterson@users.noreply.github.com>
2026-01-20 20:21:27 -0500

    version label fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 5
-        versionName = "0.5"
+        versionName = "0.51"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
