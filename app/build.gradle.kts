0a40e43c40d0ed24d78250f1e80f48676ac92e7f
Joshua Peterson <8072691+jcpeterson@users.noreply.github.com>
2026-02-09 18:58:00 -0500

    fixed settings colors in light mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 7
-        versionName = "0.73"
+        versionName = "0.73.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
